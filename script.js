83b3dd1ad3bdaa1c5c52c0622a78bac4cafad5f3
Marko <202842510+Dema2101@users.noreply.github.com>
2026-02-03 10:50:34 +0100

    IMplementacija logike crtanaj po divovima

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const container = document.querySelector("#container");
 
 
 function createGrid(velicina){
+  const adjust = document.createElement("div");
 for (let index = 0; index < velicina; index++) {
-  
   const columns = document.createElement("div");
   for (let index2 = 0; index2 < velicina; index2++) {
     const rows = document.createElement("div");
@@ -13,11 +13,40 @@ for (let index = 0; index < velicina; index++) {
     rows.style.width=`${480/velicina}px`;
     rows.style.height=`${480/velicina}px`;
     rows.style.display="inline-block";
-
+    rows.style.verticalAlign = "top";
     columns.appendChild(rows);
+
   }
   container.appendChild(columns);
 }
+
+container.appendChild(adjust);
+container.style.display="flex";
+adjust.textContent="Hello";
+
 };
 
+
+function hovering(){
+  const cells=document.querySelectorAll(".grid");
+  cells.forEach(cell => {
+
+  cell.addEventListener("mouseover", () => {
+
+
+let x = Math. floor(Math. random() * 256);
+let y = Math. floor(Math. random() * 256);
+let z = Math. floor(Math. random() * 256);
+    
+    cell.style.backgroundColor = `rgb(${x},${y},${z})`;
+    
+});
+
+});
+
+}
+
+
+
 createGrid(8);
+hovering();
