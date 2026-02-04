c0f1f2dd82bcf71dd09302236451538b2eea5c4b
Marko <202842510+Dema2101@users.noreply.github.com>
2026-02-04 12:48:19 +0100

    Implementacija logike 4.0

--- script.js
+++ script.js
@@ -1,30 +1,73 @@
-const container = document.querySelector("#container");
-const sizeBtn = document.createElement("button");
-sizeBtn.textContent="Veličina";
+let velicina = 16;
 
+const controls = document.querySelector("#controls"); 
+controls.style.marginTop="10vh";
+controls.style.marginBottom="20px";
+controls.style.display="flex";
+controls.style.justifyContent="center";
+controls.style.gap="15px";
+
+
+const infoInput = document.createElement("p");  /* TEKST ZA VELICINU */
+infoInput.textContent="Unesi SIZE";
+controls.appendChild(infoInput);
+
+
+const sizeInput = document.createElement("input"); /* INPUT ZA VELICINU */
+sizeInput.type = "range";
+sizeInput.value = "16";
+sizeInput.id = "tentacles";
+sizeInput.min = "16";
+sizeInput.max = "64";
+sizeInput.style.width="170px";
+sizeInput.style.alignSelf="center";
+controls.appendChild(sizeInput);
+
+
+const adjustBtn = document.createElement("button");  /* BTN ZA VELICINU */
+controls.appendChild(adjustBtn);
+adjustBtn.textContent="SEND IT";
+adjustBtn.style.textAlign = "center";
+adjustBtn.style.marginBottom = "5px";
+adjustBtn.style.width = "auto";
+adjustBtn.style.alignSelf = "center";
+
+
+
+adjustBtn.addEventListener("click", () => {                   /* EVENT ZA PROMJENU VELICINE */
+velicina = Number(document.getElementById("tentacles").value);
+createGrid(velicina);
+hovering();
+});
+
+
+
+
+
+
+
+
+
+const container = document.querySelector("#container");
 
 function createGrid(velicina){
-  const adjust = document.createElement("div");
+   container.innerHTML = ""; 
 for (let index = 0; index < velicina; index++) {
   const columns = document.createElement("div");
   for (let index2 = 0; index2 < velicina; index2++) {
     const rows = document.createElement("div");
     rows.classList.add("grid");
     rows.style.border = "1px solid pink";
-    rows.style.width=`${480/velicina}px`;
-    rows.style.height=`${480/velicina}px`;
+    const cellSize = 960 / velicina;
+    rows.style.width=`${cellSize}px`;
+    rows.style.height=`${cellSize}px`;
     rows.style.display="inline-block";
     rows.style.verticalAlign = "top";
     columns.appendChild(rows);
-
+    columns.style.lineHeight = "0";
   }
   container.appendChild(columns);
 }
-
-container.appendChild(adjust);
-container.style.display="flex";
-adjust.textContent="Hello";
-
 };
 
 
@@ -42,8 +85,8 @@ function hovering(){
     cell.style.backgroundColor = `rgb(${x},${y},${z})`;
   }
 
-    cells.forEach(cell => { 
-      cell.addEventListener("mousedown", (e)=> {
+      cells.forEach(cell => { 
+       cell.addEventListener("mousedown", (e)=> {
         e.preventDefault();
         isDrawing=true;
         paint(cell);
@@ -56,13 +99,10 @@ function hovering(){
     
     });
  
-
-    document.addEventListener("mouseup", () => {
-      isDrawing=false;
-    });
-
+      document.onmouseup = () => { isDrawing = false; };
 }
 
 
-createGrid(16);
+
+createGrid(velicina);
 hovering();
